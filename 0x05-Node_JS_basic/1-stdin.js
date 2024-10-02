485ac4b808dfc766a5dec717b437013c2588924e
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2024-10-02 22:56:42 +0100

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,14 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (input) => {
+    console.log('Your name is:', input.trim());
     process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+});
+
+process.on('exit', () => {
+    if (!process.stdin.isTTY) {
+        console.log('This important software is now closing');
+    }
+});
