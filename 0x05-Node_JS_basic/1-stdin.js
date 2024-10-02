ad94886fe9dc234a3c032a7c5ccaf9f2681e3ae0
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2024-10-02 22:45:53 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,16 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (input) => {
-    console.log('Your name is:', input.trim());
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
     process.exit();
-});
-
-process.on('exit', () => {
-    if (!process.stdin.isTTY) {
-        console.log('This important software is now closing');
-    }
-});
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
