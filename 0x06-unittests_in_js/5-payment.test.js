988106df538d7bb5326b07c8f067276c9bfb522a
Abdellah Taghouni <abdellahtaghouni@gmail.com>
2024-10-03 23:18:07 +0100

    Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToAp', function() {
+  beforeEach(function() {
+    sinon.spy(console, "log");
+  });
+  afterEach(function() {
+    sinon.restore();
+  });
+  it("with 100 and 20", function() {
+    sendPaymentRequestToApi(100, 20);
+    expect(console.log.calledOnce).to.be.true;
+    expect(console.log.callCount).to.be.equal(1);
+    expect(console.log.calledWith('The total is: 120')).to.be.true;
+  });
+  it("with 10 and 20", function() {
+    sendPaymentRequestToApi(10, 10);
+    expect(console.log.calledOnce).to.be.true;
+    expect(console.log.callCount).to.be.equal(1);
+    expect(console.log.calledWith('The total is: 20')).to.be.true;
+  });
+});
