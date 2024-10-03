69f6a4088ef3e9ebb859746dac4a4c64ef15219a
abdellha <abdellahtaghouni@gmail.com>
2024-10-03 00:30:36 +0000

    fix eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (input) => {
-    console.log('Your name is:', input.trim());
-    process.exit();
+  console.log('Your name is:', input.trim());
+  process.exit();
 });
 
 process.on('exit', () => {
-    if (!process.stdin.isTTY) {
-        console.log('This important software is now closing');
-    }
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+  }
 });
